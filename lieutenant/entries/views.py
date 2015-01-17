8309c29ccf2d6a8b28221502d5ce8e1f34db1a96
Erich Schroeter <erich.schroeter@gmail.com>
2015-01-17 14:52:44 -0600

    Added index view to views.py to pass 5 last created entries.

--- lieutenant/entries/views.py
+++ lieutenant/entries/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from entries.models import Entry
+
+def index(request):
+    entries_list = Entry.objects.order_by('-created_at')[:5]
+    template = loader.get_template('entries/index.html')
+    context = RequestContext(request, {
+        'entries_list': entries_list,
+    })
+    return HttpResponse(template.render(context))
