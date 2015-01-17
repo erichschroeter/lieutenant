4274e0b918c060c373a6da34856400ba069683bb
Erich Schroeter <erich.schroeter@gmail.com>
2015-01-17 14:44:40 -0600

    Registered Entry model for admin and specified fields to display on admin page.

--- lieutenant/entries/admin.py
+++ lieutenant/entries/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from entries.models import Entry
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('text', 'created_at', 'updated_at')
+    list_filter = ['created_at']
+
+admin.site.register(Entry, EntryAdmin)
