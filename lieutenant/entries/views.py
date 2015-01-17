a5d7fcaa8294e4f3b3aaed8af1254fcfe7d74aa9
Erich Schroeter <erich.schroeter@gmail.com>
2015-01-17 14:54:11 -0600

    Added detail view to views.py.

--- lieutenant/entries/views.py
+++ lieutenant/entries/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from django.template import RequestContext, loader
 
 from entries.models import Entry
@@ -8,5 +9,13 @@ def index(request):
     template = loader.get_template('entries/index.html')
     context = RequestContext(request, {
         'entries_list': entries_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, entry_id):
+    entry = get_object_or_404(Entry, id=entry_id)
+    template = loader.get_template('entries/detail.html')
+    context = RequestContext(request, {
+        'entry': entry,
     })
     return HttpResponse(template.render(context))
