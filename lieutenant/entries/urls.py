fd33eeaa2d6d8ae000337e4174046810025cec4f
Erich Schroeter <erich.schroeter@gmail.com>
2015-01-17 14:57:18 -0600

    Added create url to urls.py.

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, url
 
+from django.views.generic.edit import CreateView
+
 from entries import views
+from entries.models import Entry
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<entry_id>\d+)/$', views.detail, name='detail'),
+    url(r'^create/$', CreateView.as_view(model=Entry, success_url="/entries/"), name='create'),
 )
