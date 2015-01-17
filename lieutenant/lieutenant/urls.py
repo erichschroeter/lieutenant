55045af4662d5fb05308498d5cd20dc837a3ba57
Erich Schroeter <erich.schroeter@gmail.com>
2015-01-17 14:59:18 -0600

    Added entries urls to project urls.py.

--- lieutenant/lieutenant/urls.py
+++ lieutenant/lieutenant/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from entries import urls
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lieutenant.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^entries/', include('entries.urls', namespace="entries")),
 )
