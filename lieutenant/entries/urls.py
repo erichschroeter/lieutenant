0ce7f8a11913490f83b9fe85c4f90ff837a0f1d2
Erich Schroeter <erich.schroeter@gmail.com>
2015-01-18 10:24:33 -0600

    Added delete url to urls.py.

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from django.views.generic.edit import CreateView
 
 from entries import views
-from entries.views import EntryUpdate
+from entries.views import EntryUpdate, EntryDelete
 from entries.models import Entry
 
 urlpatterns = patterns('',
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^(?P<entry_id>\d+)/$', views.detail, name='detail'),
     url(r'^create/$', CreateView.as_view(model=Entry, success_url="/entries/"), name='create'),
     url(r'^update/(?P<pk>\d+)/$', EntryUpdate.as_view(success_url="/entries/"), name='update'),
+    url(r'^delete/(?P<pk>\d+)/$', EntryDelete.as_view(), name='delete'),
 )
