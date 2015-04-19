4aec73d7000d7184a8ab4c9041d8e1b97dc36fbc
Erich Schroeter <erich.schroeter@gmail.com>
2015-04-19 15:00:14 -0500

    Using EntryList in urls.py

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import patterns, url
 from django.views.generic.edit import CreateView
 
 from entries import views
-from entries.views import EntryCreate, EntryRead, EntryUpdate, EntryDelete
+from entries.views import EntryList, EntryCreate, EntryRead, EntryUpdate, EntryDelete
 from entries.models import Entry
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', EntryList.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', EntryRead.as_view(), name='detail'),
     url(r'^create/$', EntryCreate.as_view(success_url="/entries/"), name='create'),
     url(r'^update/(?P<pk>\d+)/$', EntryUpdate.as_view(success_url="/entries/"), name='update'),
