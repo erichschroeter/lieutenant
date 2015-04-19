dd85d57fbdeec57548d6c67e50eab6d2d0412c6e
Erich Schroeter <erich.schroeter@gmail.com>
2015-04-19 14:05:29 -0500

    Using EntryCreate in urls.py

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import patterns, url
 from django.views.generic.edit import CreateView
 
 from entries import views
-from entries.views import EntryUpdate, EntryDelete
+from entries.views import EntryCreate, EntryUpdate, EntryDelete
 from entries.models import Entry
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<entry_id>\d+)/$', views.detail, name='detail'),
-    url(r'^create/$', CreateView.as_view(model=Entry, success_url="/entries/"), name='create'),
+    url(r'^create/$', EntryCreate.as_view(success_url="/entries/"), name='create'),
     url(r'^update/(?P<pk>\d+)/$', EntryUpdate.as_view(success_url="/entries/"), name='update'),
     url(r'^delete/(?P<pk>\d+)/$', EntryDelete.as_view(), name='delete'),
 )
