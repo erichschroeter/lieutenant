d141fb32125e39a312a4390eb108593dfb62b111
Erich Schroeter <erich.schroeter@gmail.com>
2015-04-19 15:01:21 -0500

    Removed unused imports in views.py

--- lieutenant/entries/views.py
+++ lieutenant/entries/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse_lazy
-from django.template import RequestContext, loader
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
