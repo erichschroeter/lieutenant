2fd00622446dfc4bcfb0e6635ae40a21c861efd0
Erich Schroeter <erich.schroeter@gmail.com>
2015-04-19 14:50:27 -0500

    Using EntryRead in urls.py

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, url
 from django.views.generic.edit import CreateView
 
 from entries import views
-from entries.views import EntryCreate, EntryUpdate, EntryDelete
+from entries.views import EntryCreate, EntryRead, EntryUpdate, EntryDelete
 from entries.models import Entry
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<entry_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/$', EntryRead.as_view(), name='detail'),
     url(r'^create/$', EntryCreate.as_view(success_url="/entries/"), name='create'),
     url(r'^update/(?P<pk>\d+)/$', EntryUpdate.as_view(success_url="/entries/"), name='update'),
     url(r'^delete/(?P<pk>\d+)/$', EntryDelete.as_view(), name='delete'),
