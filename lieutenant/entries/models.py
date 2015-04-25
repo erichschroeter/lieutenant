daccd3875eab4330fcc7942a8525eb2427ecf66d
Erich Schroeter <erich.schroeter@gmail.com>
2015-04-25 11:57:23 -0500

    Tags field is now allowed to be blank

--- lieutenant/entries/models.py
+++ lieutenant/entries/models.py
@@ -5,5 +5,5 @@ from taggit.managers import TaggableManager
 class Entry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
     text = models.TextField(blank=True)
