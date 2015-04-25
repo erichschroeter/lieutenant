ac2ba1f21320b8564f281170c61dd8c7cba11ef1
Erich Schroeter <erich.schroeter@gmail.com>
2015-04-25 12:15:39 -0500

    Added initial entries/serializers.py

---
+++ lieutenant/entries/serializers.py
@@ -0,0 +1,15 @@
+from django.forms import widgets
+
+from rest_framework import serializers
+
+from taggit_serializer.serializers import (TagListSerializerField, TaggitSerializer)
+
+from entries.models import Entry
+
+class EntrySerializer(TaggitSerializer, serializers.ModelSerializer):
+    tags = TagListSerializerField()
+
+    class Meta:
+	model = Entry
+	fields = ('created_at', 'updated_at', 'text', 'tags')
+
