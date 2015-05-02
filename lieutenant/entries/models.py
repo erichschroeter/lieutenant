599b22d50b6a30bf1a3042d1521d0859e69843e0
Erich Schroeter <erich.schroeter@gmail.com>
2015-05-02 11:09:11 -0500

    Added slug field to Entry model

--- lieutenant/entries/models.py
+++ lieutenant/entries/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 
 from taggit.managers import TaggableManager
 
+from randomslugfield import RandomSlugField
+
 class Entry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
     user = models.ForeignKey(User)
+    # 62 possible chars ^ 5 length = 916,132,832 possible slugs
+    slug = RandomSlugField(length=5)
     tags = TaggableManager(blank=True)
     text = models.TextField(blank=True)
