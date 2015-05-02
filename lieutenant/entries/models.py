8d352978100cd68a0160ed537709f17cb9b2eb61
Erich Schroeter <erich.schroeter@gmail.com>
2015-05-02 10:37:05 -0500

    Added User foreign key to Entry model

--- lieutenant/entries/models.py
+++ lieutenant/entries/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from taggit.managers import TaggableManager
 
 class Entry(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
+    user = models.ForeignKey(User)
     tags = TaggableManager(blank=True)
     text = models.TextField(blank=True)
