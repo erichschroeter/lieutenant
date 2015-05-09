9e3ca538ec3c020864ebd9d28d75d7244128af53
Erich Schroeter <erich.schroeter@gmail.com>
2015-05-09 11:22:17 -0500

    Moved read entry view to after other views
    This fixes an issue where there may be slugs that actually match the
    other views. For example, there may be slugs that spell create,
    update, or delete.

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -8,8 +8,8 @@ from entries.models import Entry
 
 urlpatterns = patterns('',
     url(r'^$', EntryList.as_view(), name='index'),
-    url(r'^(?P<slug>[^/]+)/$', EntryRead.as_view(), name='detail'),
     url(r'^create/$', EntryCreate.as_view(success_url="/entries/"), name='create'),
     url(r'^update/(?P<slug>[^/]+)/$', EntryUpdate.as_view(success_url="/entries/"), name='update'),
     url(r'^delete/(?P<slug>[^/]+)/$', EntryDelete.as_view(), name='delete'),
+    url(r'^(?P<slug>[^/]+)/$', EntryRead.as_view(), name='detail'),
 )
