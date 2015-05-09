b797d872d3e0c4371d07fddfadfdd6862da6ed44
Erich Schroeter <erich.schroeter@gmail.com>
2015-05-09 11:24:36 -0500

    Return 403 Forbidden response for entry views if not the expected user
    Users should only be able to create, read, update, or delete entries
    that belong to them.

--- lieutenant/entries/views.py
+++ lieutenant/entries/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
 
 from entries.models import Entry
 
@@ -23,14 +24,45 @@ class EntryCreate(LoginRequiredMixin, CreateView):
     fields = ['text', 'tags']
     template_name_suffix = '_form'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(EntryCreate, self).form_valid(form)
+
 class EntryRead(LoginRequiredMixin, DetailView):
     model = Entry
     template_name_suffix = '_detail'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # Users should only be allowed to see their own entries
+        if self.object.user == self.request.user:
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+        else:
+            return HttpResponseForbidden()
 
 class EntryUpdate(LoginRequiredMixin, UpdateView):
     model = Entry
     template_name_suffix = '_update_form'
 
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # Users should only be allowed to see their own entries
+        if self.object.user == self.request.user:
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+        else:
+            return HttpResponseForbidden()
+
 class EntryDelete(LoginRequiredMixin, DeleteView):
     model = Entry
     success_url = reverse_lazy('entries:index')
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # Users should only be allowed to see their own entries
+        if self.object.user == self.request.user:
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+        else:
+            return HttpResponseForbidden()
