c4b4573b66e05920f58ee432ce5ab4ac41910586
Erich Schroeter <erich.schroeter@gmail.com>
2015-05-13 18:29:32 -0500

    Removed commented class

--- lieutenant/api/views.py
+++ lieutenant/api/views.py
@@ -20,20 +20,6 @@ class EntryMixin(object):
 class EntryList(EntryMixin, ListCreateAPIView):
     pass
 
-#class EntryList(APIView):
-
-    #def get(self, request, format=None):
-	#entries = Entry.objects.all()
-	#serializer = EntrySerializer(entries, many=True)
-	#return Response(serializer.data)
-
-    #def post(self, request, format=None):
-	#serializer = EntrySerializer(data=request.data)
-	#if serializer.is_valid():
-	    #serializer.save()
-	    #return Response(serializer.data, status=status.HTTP_201_CREATED)
-	#return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class EntryRead(APIView):
     model = Entry
 
