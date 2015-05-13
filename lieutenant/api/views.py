6868a93b716aed55290b642618844df51675423f
Erich Schroeter <erich.schroeter@gmail.com>
2015-05-13 18:30:49 -0500

    Refactored EntryRead to EntryDetail api view to use RetrieveUpdateDestroyAPIView

--- lieutenant/api/views.py
+++ lieutenant/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from entries.models import Entry
 from entries.serializers import EntrySerializer
@@ -20,30 +20,6 @@ class EntryMixin(object):
 class EntryList(EntryMixin, ListCreateAPIView):
     pass
 
-class EntryRead(APIView):
-    model = Entry
-
-    def get_object(self, pk):
-        try:
-            return Entry.objects.get(pk=pk)
-        except Entry.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        entry = self.get_object(pk)
-        serializer = EntrySerializer(entry)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        entry = self.get_object(pk)
-        serializer = EntrySerializer(entry, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serialize.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        entry = self.get_object(pk)
-        entry.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class EntryDetail(EntryMixin, RetrieveUpdateDestroyAPIView):
+    lookup_field = 'slug'
 
