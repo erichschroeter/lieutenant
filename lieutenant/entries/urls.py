907a176f988292d6eb3f4b9ec25be2976d221d50
Erich Schroeter <erich.schroeter@gmail.com>
2015-09-16 18:03:28 -0500

    Added clone support to urls

--- lieutenant/entries/urls.py
+++ lieutenant/entries/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls import patterns, url
 from django.views.generic.edit import CreateView
 
 from entries import views
-from entries.views import EntryList, EntryCreate, EntryRead, EntryUpdate, EntryDelete
+from entries.views import EntryList, EntryCreate, EntryRead, EntryUpdate, EntryDelete, EntryClone
 from entries.models import Entry
 
 urlpatterns = patterns('',
     url(r'^$', EntryList.as_view(), name='index'),
     url(r'^create/$', EntryCreate.as_view(success_url="/entries/"), name='create'),
+    url(r'^clone/(?P<slug>[^/]+)/$', EntryClone.as_view(success_url="/entries/"), name='clone'),
     url(r'^update/(?P<slug>[^/]+)/$', EntryUpdate.as_view(success_url="/entries/"), name='update'),
     url(r'^delete/(?P<slug>[^/]+)/$', EntryDelete.as_view(), name='delete'),
     url(r'^(?P<year>(\d\d\d\d))/$', views.EntryListByYear.as_view(), name='list-year'),
