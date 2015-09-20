2210f898749da7bd38c22959cf816d9b36e8a3fd
Erich Schroeter <erich.schroeter+git@gmail.com>
2015-09-20 17:17:36 -0500

    Added home url to lieutenant/urls.py

--- lieutenant/lieutenant/urls.py
+++ lieutenant/lieutenant/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from lieutenant.views import Home
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lieutenant.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^api/', include('api.urls', namespace="api")),
