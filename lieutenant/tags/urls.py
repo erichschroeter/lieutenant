0ae40c4bd1b66a34e5f4ce64e1d807c060929985
Erich Schroeter <erich.schroeter+git@gmail.com>
2015-10-07 19:23:10 -0500

    Changed order of urls to avoid special cases
    If 'update' or 'delete' tags existed the urls would not work.

--- lieutenant/tags/urls.py
+++ lieutenant/tags/urls.py
@@ -8,7 +8,7 @@ from taggit.managers import TaggableManager
 
 urlpatterns = patterns('',
     url(r'^$', views.TagList.as_view(), name='index'),
-    url(r'^(?P<slug>[^/]+)/$', views.TagRead.as_view(), name='detail'),
     url(r'^update/(?P<slug>[^/]+)/$', views.TagUpdate.as_view(success_url="/tags/"), name='update'),
     url(r'^delete/(?P<slug>[^/]+)/$', views.TagDelete.as_view(), name='delete'),
+    url(r'^(?P<slug>[^/]+)/$', views.TagRead.as_view(), name='detail'),
 )
