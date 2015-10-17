322a1eec7832ca61d021bdfa7c4a503a35c07fe4
Erich Schroeter <erich.schroeter+git@gmail.com>
2015-10-17 16:55:13 -0500

    Added count variable to context in TagList and TagRead views

--- lieutenant/tags/views.py
+++ lieutenant/tags/views.py
@@ -6,7 +6,7 @@ from django.views.generic.edit import UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 
-from entries.models import EntryTag
+from entries.models import EntryTag, TaggedEntry
 
 class LoginRequiredMixin(object):
     @classmethod
@@ -20,16 +20,20 @@ class TagList(LoginRequiredMixin, ListView):
     def get_queryset(self):
         # get all tags
         queryset = EntryTag.objects.all()
+	for tag in queryset:
+	    count = len(TaggedEntry.objects.filter(tag__name=tag.name))
+	    tag.count = count
         return queryset
 
 class TagRead(LoginRequiredMixin, DetailView):
     model = EntryTag
     template_name_suffix = '_detail'
 
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        context = self.get_context_data(object=self.object)
-        return self.render_to_response(context)
+    def get_context_data(self, **kwargs):
+        context = super(TagRead, self).get_context_data(**kwargs)
+	count = len(TaggedEntry.objects.filter(tag=self.object.name))
+	context.update({ 'count': count })
+	return context
 
 class TagUpdate(LoginRequiredMixin, UpdateView):
     model = EntryTag
