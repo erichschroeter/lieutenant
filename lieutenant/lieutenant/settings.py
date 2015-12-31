b0483ff805ab7ea0a313c4f74fab9b2212e2dd7b
Erich Schroeter <erich.schroeter@gmail.com>
2015-12-31 14:38:19 -0600

    Set STATIC_ROOT location in settings.py
    This should be configurable an environment variable.

--- lieutenant/lieutenant/settings.py
+++ lieutenant/lieutenant/settings.py
@@ -131,3 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Location where the collectstatic command will place static files
+STATIC_ROOT = '/home/lieutenant/static'
