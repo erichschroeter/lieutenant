b31e9e08f6ae88b842a340bab9559c5f41d94044
Erich Schroeter <erich.schroeter@gmail.com>
2015-12-31 09:29:51 -0600

    Added postgresql database config to settings.py

--- lieutenant/lieutenant/settings.py
+++ lieutenant/lieutenant/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'lieutenant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lieutenant',
+        'USER': 'django_lieutenant',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '', # Set to empty string for default
     }
 }
 
