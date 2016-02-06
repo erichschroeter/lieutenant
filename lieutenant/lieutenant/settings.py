4cc0617ca9e70486999f16c0dbc14c76650e5ba6
Erich Schroeter <erich.schroeter@gmail.com>
2016-02-06 07:33:51 -0600

    Commented out postgres database backend while developing
    Working with sqlite3 database is much easier while developing since I
    can easily wipe out a corrupt database by deleting the file. we can
    switch back to postgres once modifying the models calms down.

--- lieutenant/lieutenant/settings.py
+++ lieutenant/lieutenant/settings.py
@@ -103,18 +103,18 @@ WSGI_APPLICATION = 'lieutenant.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':     os.environ.get("LIEUTENANT_DB_NAME", 'lieutenant'),
-        'USER':     os.environ.get("LIEUTENANT_DB_USER", 'django_lieutenant'),
-        'PASSWORD': os.environ.get("LIEUTENANT_DB_PASSWORD", ''),
-        'HOST':     os.environ.get("LIEUTENANT_DB_HOST", 'localhost'),
-        'PORT':     os.environ.get("LIEUTENANT_DB_PORT", ''), # Set to empty string for default
-    }
     #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME':     os.environ.get(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME':     os.environ.get("LIEUTENANT_DB_NAME", 'lieutenant'),
+        #'USER':     os.environ.get("LIEUTENANT_DB_USER", 'django_lieutenant'),
+        #'PASSWORD': os.environ.get("LIEUTENANT_DB_PASSWORD", ''),
+        #'HOST':     os.environ.get("LIEUTENANT_DB_HOST", 'localhost'),
+        #'PORT':     os.environ.get("LIEUTENANT_DB_PORT", ''), # Set to empty string for default
     #}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':     os.environ.get(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Internationalization
